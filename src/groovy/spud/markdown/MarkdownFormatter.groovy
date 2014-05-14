83d439f34911ba266713de4204307c5f8f69e4aa
David Estes <davydotcom@gmail.com>
2014-05-14 15:27:07 -0400

    cleanup

--- src/groovy/spud/markdown/MarkdownFormatter.groovy
+++ src/groovy/spud/markdown/MarkdownFormatter.groovy
@@ -1,14 +1,18 @@
 package spud.markdown
 
 import spud.core.FormatterInterface
-import org.pegdown.PegDownProcessor
+import org.pegdown.*
 
 class MarkdownFormatter implements FormatterInterface {
 	String compile(String input) {
 		if(!input) {
 			return input
 		}
-		def processor = new PegDownProcessor()
+		def processor = new PegDownProcessor(org.pegdown.Extensions.FENCED_CODE_BLOCKS | org.pegdown.Extensions.HARDWRAPS | org.pegdown.Extensions.AUTOLINKS | org.pegdown.Extensions.TABLES )
 		return processor.markdownToHtml(input)
+		/*RootNode astRoot = processor.parseMarkdown(input)*/
+		/*String html = new MyCustomToHtmlSerializer().toHtml(astRoot);*/
+
+		/*.markdownToHtml(input)*/
 	}
 }
