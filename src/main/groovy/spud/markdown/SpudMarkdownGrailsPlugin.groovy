7b3ff3cfec55a8a66a0a3b2c9d6f9bcb2bd97151
William Chu <will.chu@me.com>
2018-08-15 23:02:01 -0700

    Grails 3.3.x compatible

--- src/main/groovy/spud/markdown/SpudMarkdownGrailsPlugin.groovy
+++ src/main/groovy/spud/markdown/SpudMarkdownGrailsPlugin.groovy
@@ -12,7 +12,7 @@ import org.springframework.core.env.PropertySource
 class SpudMarkdownGrailsPlugin extends Plugin {
 
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "3.1.12 > *"
+    def grailsVersion = "3.3.7 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp"
